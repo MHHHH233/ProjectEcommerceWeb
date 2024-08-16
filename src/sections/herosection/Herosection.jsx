bd222d1d837628cafef3537ea4a3c2e54ae2e281
MHHHH233 <152714429+MHHHH233@users.noreply.github.com>
2024-08-16 01:41:18 +0100

    fontsize change

--- src/sections/herosection/Herosection.jsx
+++ src/sections/herosection/Herosection.jsx
@@ -14,7 +14,7 @@ export default function Herosession() {
       <motion.div style={style} className="row "  initial={{opacity:0, x:-10}} animate={{opacity:1, x:0}} transition={{duration:2, delay:0.5}}>
       <div className="col-lg-12 col-12 px-3 text-canter">
     <h1 className="fw-bolder text-lg-start text-sm-center text text-light">Welcome to Your Ultimate </h1> <i className="fw-bolder text-light display-5">Shopping Destination</i>
-    <p className="col-6 text-light text-start mt-5  " style={{fontSize:'25px'}}>Is your cart feeling empty?
+    <p className="col-6 text-light text-start mt-5  " style={{fontSize:'28px'}}>Is your cart feeling empty?
       Visit our site, where every product feels heavenly.
       We’ve got deals and steals that will blow your mind,
       Come shop with us and see what treasures you’ll find.
